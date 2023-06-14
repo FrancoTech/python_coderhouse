550a28ae613ff17c30b4e9b8f2fd883c19cf0e6f
FrancoTech <franco.techera92@gmail.com>
2023-06-13 22:24:15 -0300

    Commit 2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # python_coderhouse
+### Resúmen
+En la carpeta Techera de este repositorio se encuentra la primera entrega para el proyecto.
+Una vez copiado el repo, para generar Test data se puede correr
+Estos comandos:
+pip install faker
+python test_data.py
+
+Una vez corrido: python manage.py runserver
+Se vera en inicio los post generados con el archivo test_data.py si se busca cualquier palabra de las que se ve en los post, se filtraran esos posts.
+- Si se quiere crear mas posts, en el menu se puede apretar la opcion Crear Posts, luego ingresar la data.
+- Si se quiere crear mas usuarios en el menu se puede apretar la opcion Crear Usuario, luego ingresar la data.
+- Si se quiere crear perfiles que correspondan a un usuario en el menu se puede apretar la opcion Definir Perfil, luego ingresar la data.
---
+++ Techera/Techera/test_data.py
@@ -0,0 +1,34 @@
+import os
+import django
+from random import choice
+from faker import Faker
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Techera.settings')
+django.setup()
+
+from TecheraAPP.models import Usuario, Perfil, BlogPost
+from datetime import date
+
+
+fake = Faker()
+
+def create_users_profiles_blogposts():
+
+    for i in range(3):
+        # Create Usuario
+        usuario = Usuario(nombre=fake.name(), email=fake.email(), password=fake.password())
+        usuario.save()
+
+        # Create BlogPosts
+        for j in range(5):
+            titulo = fake.sentence(nb_words=3)
+            descripcion = fake.paragraph(nb_sentences=2)
+            fecha_creado = fake.date_between(start_date='-1y', end_date='today')
+
+            blogpost = BlogPost(usuario=usuario, titulo=titulo, descripcion=descripcion, fecha_creado=fecha_creado)
+            blogpost.save()
+
+    print('Data added successfully!')
+
+if __name__ == '__main__':
+    create_users_profiles_blogposts()
